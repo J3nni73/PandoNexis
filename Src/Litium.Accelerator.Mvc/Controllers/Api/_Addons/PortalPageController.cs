f6d1e3659e2be435da8570213676ba8d2bee8bd3
Jennifer Sköld <j3nni73@gmail.com>
2023-03-09 20:50:32 +0100

    api controller

---
+++ Src/Litium.Accelerator.Mvc/Controllers/Api/_Addons/PortalPageController.cs
@@ -0,0 +1,46 @@
+﻿using Litium.Media;
+using Litium.Runtime;
+using Microsoft.AspNetCore.Mvc;
+using PandoNexis.Accelerator.Extensions.Builders.Media;
+using PandoNexis.AddOns.Extensions.PNPortalPage;
+using System;
+using System.Threading.Tasks;
+
+namespace Litium.Accelerator.Mvc.Controllers.Api.Addons
+{
+    [Route("api/portalpage")]
+    public class PortalPageController : ApiControllerBase
+    {
+
+        private readonly PortalPageService _portalPageService;
+        private readonly ApplicationJsonConverter _applicationJsonConverter;
+
+        public PortalPageController(PortalPageService portalPageService, ApplicationJsonConverter applicationJsonConverter)
+        {
+            _portalPageService = portalPageService;
+            _applicationJsonConverter = applicationJsonConverter;
+        }
+
+        [HttpGet]
+        [Route("getGetPortalPageData")]
+        public async Task<IActionResult> GetPortalPageData(Guid PageSystemId)
+        {
+            var portalPageData = _portalPageService.BuildPortalPageData(PageSystemId);
+
+            if (portalPageData != null)
+            {
+                try
+                {
+                    var returnData = _applicationJsonConverter.ConvertObjectToJson(portalPageData);
+                    return Ok(returnData);
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+                
+            }
+            return BadRequest();
+        }
+    }
+}
