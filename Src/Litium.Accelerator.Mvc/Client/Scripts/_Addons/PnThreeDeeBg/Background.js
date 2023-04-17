230fe61c87e6309688a752d2b4dcfc3e41e59468
raspenberg <42236369+raspenberg@users.noreply.github.com>
2023-04-17 12:26:03 +0200

    Updated NoErp

--- Src/Litium.Accelerator.Mvc/Client/Scripts/_Addons/PnThreeDeeBg/Background.js
+++ Src/Litium.Accelerator.Mvc/Client/Scripts/_Addons/PnThreeDeeBg/Background.js
@@ -8,7 +8,7 @@ import WAVES from "./vendor/vanta.waves";
 import * as THREE from 'three';
 const Background = (props) => {
     const [vantaEffect, setVantaEffect] = useState(0);
-    const [theme, setTheme] = useState(window.channel && window.channel === 'siteclean' ? 'waves' : props.theme || 'waves');
+    const [theme, setTheme] = useState(window.channel && window.channel === 'sitenoerp' ? 'waves' : props.theme || 'waves');
     
     const mainBg = useRef(null);
     const cellsOptions = {
